7758576081651622772d2e79b9847fa06d02a759
mikoto2000 <mikoto2000@gmail.com>
2024-11-18 08:23:11 +0900

    1秒ごとにイメージが切り替わる機能を追加。

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { open } from "@tauri-apps/plugin-dialog";
 import { DirEntry, readDir } from "@tauri-apps/plugin-fs";
@@ -7,7 +7,30 @@ import { convertFileSrc } from "@tauri-apps/api/core";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
+  const [dir, setDir] = useState("");
+  const [imageEntries, setImageEntries] = useState<DirEntry[]>([]);
   const [currentImagePath, setCurrentImagePath] = useState("");
+  const [interval, _setInterval] = useState(1000);
+  const [imageIndex, setImageIndex] = useState(0);
+  let initialized = false;
+
+  useEffect(() => {
+    if (!initialized) {
+      switchImage();
+      initialized = true;
+    }
+  });
+
+  const switchImage = () => {
+    setTimeout(async () => {
+      const tmp = await path.join(dir, imageEntries[imageIndex].name);
+      setGreetMsg(tmp);
+      setCurrentImagePath(convertFileSrc(tmp));
+
+      setImageIndex((imageIndex + 1) % imageEntries.length);
+      switchImage();
+    }, interval);
+  };
 
   async function getImageEntry() {
     let dir = await open({
@@ -15,12 +38,10 @@ function App() {
       directory: true,
     });
     if (dir) {
+      setDir(dir);
       const entries = await readDir(dir);
       const filterdEntries = entries.filter((entry) => entry.isFile && isImage(entry));
-      const tmp = await path.join(dir, filterdEntries[0].name);
-      setGreetMsg(tmp);
-      setCurrentImagePath(convertFileSrc(tmp));
-
+      setImageEntries(filterdEntries);
     }
   }
 
@@ -43,6 +64,7 @@ function App() {
       </form>
       <img src={currentImagePath}></img>
       <p>{greetMsg}</p>
+      <p>{imageIndex}</p>
     </main>
   );
 }
